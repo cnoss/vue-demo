80715284999a26ce2134537dbfb9c164adc7fd3a
cnoss <christian.noss@th-koeln.de>
2021-06-22 14:55:58 +0200

    Basisversion erstell

---
+++ dist/precache-manifest.a2bab03a0f0e22c8029b84734275ff88.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ac801fe909a316ddea2",
+    "url": "/css/about.56d24118.css"
+  },
+  {
+    "revision": "e02d1f6a73e835f00676",
+    "url": "/css/app.ba04fff9.css"
+  },
+  {
+    "revision": "62cee3c5dd032f30d2b747d3c88fab24",
+    "url": "/data/440px-Alan_Turing_Aged_16.jpg"
+  },
+  {
+    "revision": "9209e0d833e9d6f1e0140dba23a90bbd",
+    "url": "/data/440px-JohnvonNeumann-LosAlamos.gif"
+  },
+  {
+    "revision": "a950e51f0d52e1072b355e7dc1e14b7e",
+    "url": "/data/Bill_Gates_2017.jpg"
+  },
+  {
+    "revision": "4d22fe317ae439a9b5d833d4d36c6ec7",
+    "url": "/data/Sir_Tim_Berners-Lee.jpg"
+  },
+  {
+    "revision": "88b92e9a57daf7a647a76176f3d790ed",
+    "url": "/data/Steve_Wozniak_by_Gage_Skidmore_3.jpg"
+  },
+  {
+    "revision": "8aff12393db13afe36d5c5a86bbaac2e",
+    "url": "/famous.json"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "9677773e59f27b26af809c9ff47e7944",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ac801fe909a316ddea2",
+    "url": "/js/about.1c8f8047.js"
+  },
+  {
+    "revision": "e02d1f6a73e835f00676",
+    "url": "/js/app.a3d39355.js"
+  },
+  {
+    "revision": "52f24fd58c7ae50e9cdc",
+    "url": "/js/chunk-vendors.ca1f08a6.js"
+  },
+  {
+    "revision": "e3535a9d9cf9718e5b89e14aabfa50eb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
